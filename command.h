08b3e55dc10cea959ae778a1805994268baa2cf1
wanwenxin <33198921+wanwenxin@users.noreply.github.com>
2017-11-30 19:28:50 +0800

    Update command.h

--- command.h
+++ command.h
@@ -15,11 +15,11 @@ int command()
 		HF.initialize(HF.text, HF.text_ch, HF.text_wight);          //初始化
 		HF.CreateHuffmanTree(HF.tree, HF.text_ch, HF.text_wight);   //创建huffman树tree[]
 		HF.write_tree_weight(HF.text_wight, HF.wight_in_text);      //int权值变为char权值
-		cout << HF.wight_in_text;
-		outfile.write(HF.wight_in_text, N+2);  //char权值写入并换行
+		outfile.write(HF.wight_in_text, N+2);       //char权值写入并换行
 		HF.HuffmanCode(HF.code, HF.tree);           //由huffman树tree[]得编码code[]
 		HF.incode(HF.code, HF.text, HF.binarycode); //根据翻译表code[]，将待编码得temp[]编译成二进制存入binarycode[]
-		outfile.write(HF.binarycode, strlen(HF.binarycode));   //将binarycode[]输出到outoutfile
+		HF.code_01(HF.binarycode, HF.temp);         //01码压缩存入temp[]
+		outfile.write(HF.temp, strlen(HF.temp));    //将binarycode[]输出到outoutfile
 
 		close_file(); //关闭文件
 	}
@@ -29,16 +29,11 @@ int command()
 
 		Huffman HF;
 		HF.read_tree_weight(HF.text_wight, HF.wight_in_text);       //从文件读入char权值，并变为int权值;
-		//cout << "1" << endl;
 		HF.initialize_simpe(HF.text_ch, HF.text_wight);             //直接初始化
-		//cout << "2" << endl;
 		HF.CreateHuffmanTree(HF.tree, HF.text_ch, HF.text_wight);   //创建huffman树tree[]
-		//cout << "3" << endl;
-		infile.getline(HF.text, 1000, '\n');                  //读入源文件文件（待翻译得01串）存入temp[]
-		//cout << HF.text << endl;
-		//cout << "4" << endl;
-		HF.decode(HF.tree, HF.text, HF.decode_ch);  //根据huffman树，将待翻译的temp[]翻译成字符串存入decode_ch[]
-		//cout << "5" << endl;
+		infile.getline(HF.temp, 1000, '\n');                        //读入源文件文件（压缩的01串）存入temp[]
+		HF.decode_01(HF.temp, HF.text);                             //还原压缩的01串
+		HF.decode(HF.tree, HF.text, HF.decode_ch);           //根据huffman树，将待翻译的temp[]翻译成字符串存入decode_ch[]
 		outfile.write(HF.decode_ch, strlen(HF.decode_ch));   //将decode_ch[]输出到outoutfile
 
 		close_file(); //关闭文件
